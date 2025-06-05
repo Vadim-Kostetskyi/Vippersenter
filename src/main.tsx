b3a5e7edad0541d073aeb565c35730484acf7319
Vadim-Kostetskyi <rasty2308@gmail.com>
2025-06-05 17:13:36 +0300

    create router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./translations/i18n.ts";
-import App from "./pages/App.tsx";
-import "./styles/index.scss";
 import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./pages/App.tsx";
+import "./translations/i18n.ts";
+import "./styles/index.scss";
 import { store } from "storeRedux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
+  <BrowserRouter>
+    <StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </StrictMode>
+  </BrowserRouter>
 );
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
+import Login from "modules/user/conteiners/Login";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/admin" element={<Login />} />
+    </Routes>
+  );
 }
 
 export default App;
