78dfb52603f1dd28a81bba48cbd263e292a0f3a1
Vadim-Kostetskyi <rasty2308@gmail.com>
2025-06-16 15:26:49 +0300

    add search input

--- src/modules/core/components/SearchButton/index.tsx
+++ src/modules/core/components/SearchButton/index.tsx
@@ -1,18 +1,50 @@
+import { useState, FC } from "react";
 import Search from "assets/svg/Search";
 import styles from "./index.module.scss";
-import { FC } from "react";
 
 interface SearchButtonProps {
   isLaptop?: boolean;
 }
 
 const SearchButton: FC<SearchButtonProps> = ({ isLaptop }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [query, setQuery] = useState("");
+
+  const toggleInput = () => setIsOpen((prev) => !prev);
+  const closeInput = () => setIsOpen(false);
+
   return (
-    <>
-      <button className={isLaptop ? styles.searchBtn : styles.searchBtnMobile}>
-        <Search className={styles.search} />
-      </button>
-    </>
+    <div className={styles.searchWrapper}>
+      {!isOpen && (
+        <button
+          className={isLaptop ? styles.searchBtn : styles.searchBtnMobile}
+          onClick={toggleInput}
+          aria-label="Open search input"
+        >
+          <Search className={styles.search} />
+        </button>
+      )}
+
+      {isOpen && (
+        <div className={styles.inputWrapper}>
+          <Search className={styles.iconLeft} />
+          <input
+            type="text"
+            autoFocus
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            className={styles.searchInput}
+          />
+          <button
+            onClick={closeInput}
+            className={styles.clearBtn}
+            aria-label="Close search input"
+          >
+            ×
+          </button>
+        </div>
+      )}
+    </div>
   );
 };
 
