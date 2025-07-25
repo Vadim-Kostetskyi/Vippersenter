ef51f2a7adf4b5d6637d529532d00a2b30d814e8
Vadim-Kostetskyi <rasty2308@gmail.com>
2025-07-25 11:06:17 +0300

    ap

--- src/modules/product/components/ProductSwiperPopular/index.tsx
+++ src/modules/product/components/ProductSwiperPopular/index.tsx
@@ -5,12 +5,48 @@ import CoreSwiper from "components/CoreSwiper";
 import { breakpoints } from "utils/constants";
 import styles from "./index.module.scss";
 import { useGetProductsQuery } from "storeRedux/productsApi";
+import { useEffect } from "react";
 
 const ProductSwiperPopular = () => {
   const { data: popularProducts } = useGetProductsQuery({
     popularProduct: true,
   });
   const { t } = useTranslation();
+
+useEffect(() => {
+  const fetchShipping = async () => {
+    try {
+      const res = await fetch(
+        "http://localhost/vise-data-base/api/v1/myAPI/api/v1/order/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            fromPostalCode: "0150",
+            toPostalCode: "5000",
+            countryCode: "NO",
+            weightKg: 1.2,
+            lengthCm: 30,
+            widthCm: 20,
+            heightCm: 10,
+          }),
+        }
+      );
+
+      const data = await res.json();
+      console.log("Response:", data);
+    } catch (err) {
+      console.error("Fetch error:", err);
+    }
+  };
+
+  fetchShipping();
+}, []);
+
+  
+
 
   return (
     <div className={styles.wrapper}>
