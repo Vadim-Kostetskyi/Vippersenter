bb1b78d0a06240e7d181ddcc1d0ee10532130806
Vadim-Kostetskyi <rasty2308@gmail.com>
2025-08-19 16:54:56 +0300

    switching between mailboxes

--- src/modules/order/containers/CustomerDetails/index.tsx
+++ src/modules/order/containers/CustomerDetails/index.tsx
@@ -1,8 +1,10 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useTranslation } from "react-i18next";
 import InputField from "components/InputField";
 import PostnordDelivery from "../PostnordDelivery";
 import { inputs } from "./data";
+import postnordLogo from 'assets/image/post/postnordLogo.webp'
+import postenLogo from "assets/image/post/postenLogo.png";
 import styles from "./index.module.scss";
 
 interface CustomerDetailsProps {
@@ -10,6 +12,7 @@ interface CustomerDetailsProps {
 }
 
 const CustomerDetails: FC<CustomerDetailsProps> = ({ setPrice }) => {
+  const [delivery, setDelivery] = useState("postnord");
   const { t } = useTranslation();  
 
   return (
@@ -25,7 +28,37 @@ const CustomerDetails: FC<CustomerDetailsProps> = ({ setPrice }) => {
             require={true}
           />
         ))}
-        <PostnordDelivery setPrice={setPrice} />
+        <div className={styles.postBox}>
+          <label className={styles.post}>
+            <input
+              type="radio"
+              name="delivery"
+              value="postnord"
+              checked={delivery === "postnord"}
+              onChange={(e) => setDelivery(e.target.value)}
+            />
+            <span>
+              <img src={postnordLogo} alt="" />
+            </span>
+          </label>
+
+          <label className={styles.post}>
+            <input
+              type="radio"
+              name="delivery"
+              value="posten"
+              checked={delivery === "posten"}
+              onChange={(e) => setDelivery(e.target.value)}
+            />
+            <span>
+              <img src={postenLogo} alt="" />
+            </span>
+          </label>
+        </div>
+
+        {delivery === "postnord" ? null : (
+          <PostnordDelivery setPrice={setPrice} />
+        )}
         <label htmlFor="orderComments" className={styles.orderComments}>
           <span>{t("order.orderNotes")}</span>
           <textarea
